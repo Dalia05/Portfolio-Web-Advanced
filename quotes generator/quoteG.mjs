eb889cc9d364faa4a6cdfd5b1a3b149a21221d79
Dalia05 <dalia.rizzo@student.ehb.be>
2024-06-04 23:03:24 +0100

    quotes generator animaties

--- quotes generator/quoteG.mjs
+++ quotes generator/quoteG.mjs
@@ -1,28 +1,34 @@
+//elementen selecteren
 let quote = document.getElementById('quote');
 let authorE = document.getElementById('author');
 let button = document.getElementById('button');
 
-(async function() {
+//async & await + arrow functions + elementen manipuleren + self executing function
+(async () => {
+    //event aan element koppelen
     button.addEventListener('click', async () => {
         let quoteData = await fetchQuote();
         displayQuote(quoteData);
         restartAnimation();
     });
 
+    //fetch om data op te halen
     const fetchQuote = async () => {
         let response = await fetch('https://api.quotable.io/random');
         let data = await response.json();
         return data;
     }
 
+    //template literals + destructuring +JSON manipuleren/weergeven
     const displayQuote = ({ content, author }) => {
         quote.textContent = `"${content}"`;
         authorE.textContent = `- ${author}`;
     }
 
     const restartAnimation = () => {
-        quote.classList.remove('animation');  // Remove the class to stop the animation
-        void quote.offsetWidth;  // Trigger a reflow to restart the animation
-        quote.classList.add('animation');  // Add the class back to restart the animation
+        quote.classList.remove('animation');
+        void quote.offsetWidth; 
+        quote.classList.add('animation'); 
     };
+
 })();
